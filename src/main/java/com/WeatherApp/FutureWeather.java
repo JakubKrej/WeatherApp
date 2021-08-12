3ceba14d4ff07036be8ff09db81dddcf2e658601
jakubkrej <jakrej@o2.pl>
2021-08-12 22:29:15 +0200

    Icons description weather

--- src/main/java/com/WeatherApp/FutureWeather.java
+++ src/main/java/com/WeatherApp/FutureWeather.java
@@ -5,41 +5,56 @@ import java.math.BigDecimal;
 public class FutureWeather {
 
     private String city;
-    private BigDecimal curentDayTemp;
+    private int curentDayTemp;
+    private String description1;
     private String day1;
-    private BigDecimal secendDayTemp;
+    private int secendDayTemp;
+    private String description2;
     private String day2;
-    private BigDecimal thirdDayTemp;
+    private int thirdDayTemp;
+    private String description3;
     private String day3;
-    private BigDecimal fourthDayTemp;
+    private int fourthDayTemp;
+    private String description4;
     private String day4;
-    private BigDecimal fifthDayTemp;
+    private int fifthDayTemp;
+    private String description5;
     private String day5;
-    private BigDecimal sixthDayTemp;
+    private int sixthDayTemp;
+    private String description6;
     private String day6;
-    private BigDecimal seventhDayTemp;
+    private int seventhDayTemp;
+    private String description7;
     private String day7;
-    private BigDecimal eighthDayTemp;
+    private int eighthDayTemp;
+    private String description8;
     private String day8;
 
-
-    public FutureWeather(String city, BigDecimal curentDayTemp, String day1, BigDecimal secendDayTemp, String day2, BigDecimal thirdDayTemp, String day3, BigDecimal fourthDayTemp, String day4, BigDecimal fifthDayTemp, String day5, BigDecimal sixthDayTemp, String day6, BigDecimal seventhDayTemp, String day7, BigDecimal eighthDayTemp, String day8) {
+    public FutureWeather(String city, int curentDayTemp, String description1, String day1, int secendDayTemp, String description2, String day2, int thirdDayTemp, String description3, String day3, int fourthDayTemp, String description4, String day4, int fifthDayTemp, String description5, String day5, int sixthDayTemp, String description6, String day6, int seventhDayTemp, String description7, String day7, int eighthDayTemp, String description8, String day8) {
         this.city = city;
         this.curentDayTemp = curentDayTemp;
+        this.description1 = description1;
         this.day1 = day1;
         this.secendDayTemp = secendDayTemp;
+        this.description2 = description2;
         this.day2 = day2;
         this.thirdDayTemp = thirdDayTemp;
+        this.description3 = description3;
         this.day3 = day3;
         this.fourthDayTemp = fourthDayTemp;
+        this.description4 = description4;
         this.day4 = day4;
         this.fifthDayTemp = fifthDayTemp;
+        this.description5 = description5;
         this.day5 = day5;
         this.sixthDayTemp = sixthDayTemp;
+        this.description6 = description6;
         this.day6 = day6;
         this.seventhDayTemp = seventhDayTemp;
+        this.description7 = description7;
         this.day7 = day7;
         this.eighthDayTemp = eighthDayTemp;
+        this.description8 = description8;
         this.day8 = day8;
     }
 
@@ -51,12 +66,20 @@ public class FutureWeather {
         this.city = city;
     }
 
-    public BigDecimal getCurentDayTemp() {
+    public int getCurentDayTemp() {
         return curentDayTemp;
     }
 
-    public void setCurentDayTemp(BigDecimal curentDayTemp) {
+    public void setCurentDayTemp(int curentDayTemp) {
         this.curentDayTemp = curentDayTemp;
+    }
+
+    public String getDescription1() {
+        return description1;
+    }
+
+    public void setDescription1(String description1) {
+        this.description1 = description1;
     }
 
     public String getDay1() {
@@ -67,12 +90,20 @@ public class FutureWeather {
         this.day1 = day1;
     }
 
-    public BigDecimal getSecendDayTemp() {
+    public int getSecendDayTemp() {
         return secendDayTemp;
     }
 
-    public void setSecendDayTemp(BigDecimal secendDayTemp) {
+    public void setSecendDayTemp(int secendDayTemp) {
         this.secendDayTemp = secendDayTemp;
+    }
+
+    public String getDescription2() {
+        return description2;
+    }
+
+    public void setDescription2(String description2) {
+        this.description2 = description2;
     }
 
     public String getDay2() {
@@ -83,12 +114,20 @@ public class FutureWeather {
         this.day2 = day2;
     }
 
-    public BigDecimal getThirdDayTemp() {
+    public int getThirdDayTemp() {
         return thirdDayTemp;
     }
 
-    public void setThirdDayTemp(BigDecimal thirdDayTemp) {
+    public void setThirdDayTemp(int thirdDayTemp) {
         this.thirdDayTemp = thirdDayTemp;
+    }
+
+    public String getDescription3() {
+        return description3;
+    }
+
+    public void setDescription3(String description3) {
+        this.description3 = description3;
     }
 
     public String getDay3() {
@@ -99,12 +138,20 @@ public class FutureWeather {
         this.day3 = day3;
     }
 
-    public BigDecimal getFourthDayTemp() {
+    public int getFourthDayTemp() {
         return fourthDayTemp;
     }
 
-    public void setFourthDayTemp(BigDecimal fourthDayTemp) {
+    public void setFourthDayTemp(int fourthDayTemp) {
         this.fourthDayTemp = fourthDayTemp;
+    }
+
+    public String getDescription4() {
+        return description4;
+    }
+
+    public void setDescription4(String description4) {
+        this.description4 = description4;
     }
 
     public String getDay4() {
@@ -115,12 +162,20 @@ public class FutureWeather {
         this.day4 = day4;
     }
 
-    public BigDecimal getFifthDayTemp() {
+    public int getFifthDayTemp() {
         return fifthDayTemp;
     }
 
-    public void setFifthDayTemp(BigDecimal fifthDayTemp) {
+    public void setFifthDayTemp(int fifthDayTemp) {
         this.fifthDayTemp = fifthDayTemp;
+    }
+
+    public String getDescription5() {
+        return description5;
+    }
+
+    public void setDescription5(String description5) {
+        this.description5 = description5;
     }
 
     public String getDay5() {
@@ -131,12 +186,20 @@ public class FutureWeather {
         this.day5 = day5;
     }
 
-    public BigDecimal getSixthDayTemp() {
+    public int getSixthDayTemp() {
         return sixthDayTemp;
     }
 
-    public void setSixthDayTemp(BigDecimal sixthDayTemp) {
+    public void setSixthDayTemp(int sixthDayTemp) {
         this.sixthDayTemp = sixthDayTemp;
+    }
+
+    public String getDescription6() {
+        return description6;
+    }
+
+    public void setDescription6(String description6) {
+        this.description6 = description6;
     }
 
     public String getDay6() {
@@ -147,12 +210,20 @@ public class FutureWeather {
         this.day6 = day6;
     }
 
-    public BigDecimal getSeventhDayTemp() {
+    public int getSeventhDayTemp() {
         return seventhDayTemp;
     }
 
-    public void setSeventhDayTemp(BigDecimal seventhDayTemp) {
+    public void setSeventhDayTemp(int seventhDayTemp) {
         this.seventhDayTemp = seventhDayTemp;
+    }
+
+    public String getDescription7() {
+        return description7;
+    }
+
+    public void setDescription7(String description7) {
+        this.description7 = description7;
     }
 
     public String getDay7() {
@@ -163,12 +234,20 @@ public class FutureWeather {
         this.day7 = day7;
     }
 
-    public BigDecimal getEighthDayTemp() {
+    public int getEighthDayTemp() {
         return eighthDayTemp;
     }
 
-    public void setEighthDayTemp(BigDecimal eighthDayTemp) {
+    public void setEighthDayTemp(int eighthDayTemp) {
         this.eighthDayTemp = eighthDayTemp;
+    }
+
+    public String getDescription8() {
+        return description8;
+    }
+
+    public void setDescription8(String description8) {
+        this.description8 = description8;
     }
 
     public String getDay8() {
